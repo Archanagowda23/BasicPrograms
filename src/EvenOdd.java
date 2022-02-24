6eb2dccabc1d34864cb713a4037e33ddb36a38c3
Archanagowda23 <archana249@gmail.com>
2022-02-24 20:43:58 +0530

    EvenOdd

---
+++ src/EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=scan.nextInt();
+        if(n%2==0)
+        {
+            System.out.println(n+ " is Even Number");
+        }
+        else
+        {
+            System.out.println(n+ " is odd Number");
+        }
+    }
+}
